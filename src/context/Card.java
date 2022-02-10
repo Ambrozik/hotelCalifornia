963df707e7540fd0e0d9566728a6b02ffc50c35c
ambrozik <hug.a@hotmail.fr>
2022-02-10 15:05:26 +0100

    card update

--- src/context/Card.java
+++ src/context/Card.java
@@ -1,17 +1,50 @@
 package context;
 
+import java.util.Objects;
+
 public class Card {
-    int id;
+    int idCard;
+    String idRoom;
 
-    public Card(int id) {
-        this.id = id;
+    /**
+     * Constructor for class Card
+     *
+     * @param idCard of card
+     * @param idRoom of room associated
+     */
+    public Card(int idCard, String idRoom) {
+        this.idCard = idCard;
+        this.idRoom = idRoom;
     }
 
-    public int getId() {
-        return id;
+
+    public int getIdCard() {
+        return idCard;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIdCard(int idCard) {
+        this.idCard = idCard;
     }
+
+    public String getIdRoom() {
+        return idRoom;
+    }
+
+    public void setIdRoom(String idRoom) {
+        this.idRoom = idRoom;
+    }
+
+    public void setCard(int idCard, String idRoom) {
+        this.idCard = idCard;
+        this.idRoom = idRoom;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "idCard=" + idCard +
+                ", idRoom='" + idRoom + '\'' +
+                '}';
+    }
+
 }
